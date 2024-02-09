9e64666ae1740d231ba925db341b033e39ad5af6
n0tnish <nishaatgafieldien@gmail.com>
2024-02-09 16:21:22 +0200

    adding

--- index.js
+++ index.js
@@ -1,1 +1,114 @@
-console.log("welcome to Express JS")
+//console.log("welcome to Express JS")
+import express from 'express'
+
+//Create an express app 
+const app = express ( )
+const port = +process.env.PORT || 4000
+// Router
+app.get('/', (req, res)=>{
+    res.status(200).json({
+            msg: 'You\'re home'
+    })
+})
+app.all('*', (req, res)=>{
+    res.json({
+            status: 404,
+            msg: '404 Page'
+    })
+})
+
+
+app.listen(port)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+/*
+HTTP METHODS
+-   GET 
+- POST
+- PUT 
+- PATCH
+- DELETE
+ */
